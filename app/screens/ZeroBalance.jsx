86ab3b5993fac45fb1484481c1b31b386cfad6ed
Divyansh_Sancheti <156776291+DivyanshSancheti@users.noreply.github.com>
2024-09-11 01:17:38 +0530

    Update ZeroBalance.jsx

--- app/screens/ZeroBalance.jsx
+++ app/screens/ZeroBalance.jsx
@@ -1,79 +1,142 @@
-import React, {
-  useEffect,
-  useContext,
-  useCallback,
-  useRef,
-  useState,
-} from "react";
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
-import { Button, Alert } from "react-native";
-import YoutubePlayer from "react-native-youtube-iframe";
-import { UserContext } from "../App";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, Pressable,ScrollView, ImageBackground, Platform } from "react-native";
 
-const Video = () => {
-  const { userData, setUserData } = useContext(UserContext);
-  const [playing, setPlaying] = useState(false);
-  const vid = ["AIOR1x7fPcQ", "OJGUYYUPH_0", "W4hcZe79qS0"];
-
-  const onStateChange = useCallback((state) => {
-    if (state === "ended") {
-      setPlaying(false);
-      Alert.alert("video has finished playing!");
-    }
-  }, []);
-
-  const togglePlaying = useCallback(() => {
-    setPlaying((prev) => !prev);
-  }, []);
-  console.log(userData.wrongQuestionId);
+const ZeroBalanceAccount = () => {
   return (
-    <SafeAreaView
-      style={{
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <View style={{ paddingHorizontal: 20 }}>
-        <Text style={{ textAlign: "center", fontSize: 30, fontWeight: 700 }}>
-          Video Tutorials
-        </Text>
-        <Text
-          style={{
-            textAlign: "center",
-            fontSize: 15,
-            paddingTop: 20,
-            paddingBottom: 20,
-          }}
-        >
-          Imporove you knowledge by learning the things that went wrong during
-          previous Level
-        </Text>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          {userData.wrongQuestion.map((ques, id) => (
-            <View key={id}>
-              <Text style={{ paddingBottom: 20, fontSize: 20 }}>
-                {id + 1}. {ques} ?
-              </Text>
-              <View style={{ borderRadius: 10 }}>
-                <YoutubePlayer
-                  height={250}
-                  play={playing}
-                  videoId={vid[id]}
-                  onChangeState={onStateChange}
-                />
-                {/* <Button
-                  title={playing ? "pause" : "play"}
-                  onPress={togglePlaying}
-                /> */}
+    <SafeAreaView style={{ paddingTop: Platform.OS === 'android' ? 40 : 0, justifyContent: "center", alignItems: "center" }}>
+      <ScrollView style={{ display: "flex"}}>
+        <View style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <ImageBackground source={require("../assets/Underp.jpg")} style={styles.topCard}>
+            <View style={styles.overlay} />
+            <View style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <TouchableOpacity>
+                <Text style={{ color: "white", fontWeight: "bold", fontSize: 50 }}>Open a Zero Balance Account</Text>
+                <Text style={{ color: "white", fontSize: 20, marginTop: 10 }}>Start Banking with Zero Deposit</Text>
+                <Text style={{ color: "white", fontSize: 17, marginTop: 10 }}>
+                  Enjoy the benefits of a zero balance account.
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+          <View style={styles.rockbottom}>
+          <View
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: 10,
+                marginTop: 0,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Pressable
+                onPress={() => navigation.navigate("Zero")}
+                style={{
+                  backgroundColor: "#FFB6C1", // Pastel pink
+                  height: 200,
+                  width: 170,
+                  borderRadius: 20,
+                }}
+              >
+                <Image source={require("../assets/piggy.png")} style={{ width: 40, height: 40, margin: 10 }} />
+                <Text
+                  style={{ color: "darkblue", marginLeft: 10, fontWeight: "600" }}
+                >
+                  Zero Balance account
+                </Text>
+                <Text
+                  style={{ color: "darkblue", margin: 10, opacity: 0.5, fontSize: 12 }}
+                >
+                  Open a Savings Bank Account and start saving today
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={() => navigation.navigate("Scheme")}
+                style={{
+                  backgroundColor: "#FFD700", // Pastel yellow
+                  height: 200,
+                  width: 170,
+                  borderRadius: 20,
+                }}
+              >
+                <Image source={require("../assets/doc.png")} style={{ width: 40, height: 40, margin: 10 }} />
+                <Text style={{ color: "brown", marginLeft: 10, fontWeight: "600" }}>
+                  Online Application
+                </Text>
+                <Text
+                  style={{ color: "brown", margin: 10, opacity: 0.5, fontSize: 12 }}
+                >
+                  Online application for Account Opening
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={() => navigation.navigate("Savings")}
+                style={{
+                  backgroundColor: "#98FB98", // Pastel green
+                  height: 200,
+                  width: 170,
+                  borderRadius: 20,
+                }}
+              >
+                <Image source={require("../assets/mobile.png")} style={{ width: 40, height: 40, margin: 10 }} />
+                <Text
+                  style={{ color: "darkgreen", marginLeft: 10, fontWeight: "600" }}
+                >
+                  Mobile Banking
+                </Text>
+                <Text
+                  style={{ color: "darkgreen", margin: 10, opacity: 0.5, fontSize: 12 }}
+                >
+                  Access mobile banking services with your account
+                </Text>
+              </Pressable>
+              <View
+                style={{
+                  backgroundColor: "#AFEEEE", // Pastel blue
+                  height: 200,
+                  width: 170,
+                  borderRadius: 20,
+                }}
+              >
+                <Image source={require("../assets/bank.png")} style={{ width: 40, height: 40, margin:10 }} />
+                <Text style={{ marginLeft: 10, fontWeight: "600" }}>
+                 Supported Banks
+                </Text>
+                <Text
+                  style={{ margin: 10, opacity: 0.5, fontSize: 12 }}>
+                   Explore various Banks that support zero balance account 
+                </Text>
               </View>
             </View>
-          ))}
-        </ScrollView>
-      </View>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default Video;
+export default ZeroBalanceAccount;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  topCard: {
+    borderRadius: 40,
+    resizeMode: "contain",
+    height: 300,
+    
+    padding: 30,
+    overflow: "hidden"
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust opacity as needed
+  },
+  rockbottom: {
+    height: 450,
+    display: "flex",
+    alignItems: "center",
+  },
+});
